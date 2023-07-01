923c1e4882041a1c0309121367ba06a7ef05265c
cassieopeanuts <92317800+cassieopeanuts@users.noreply.github.com>
2023-07-01 17:41:05 +0900

    changed ethers.js to @usedapp/core

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
-import Web3 from 'web3';
-import detectEthereumProvider from '@metamask/detect-provider';
-import logo from './logo.png';
 import mainlogoImage from './main-logo.png';
 import infoImage from './info.png';
 import loginImage from './login.png';
@@ -18,19 +14,31 @@ import tipsGivenImage from './tips-given.png';
 import tipsReceivedImage from './tips-received.png';
 import myStatsImage from './my-stats.png';
 import cassieImage from './cassie.png';
+import logo from './logo.png';
+import './App.css';
+import { useEthers, useContractFunction } from '@usedapp/core';
+import detectEthereumProvider from '@metamask/detect-provider';
+
+const placeholderContractAddress = '0x...'; // Replace with actual contract address
+const placeholderABI = []; // Replace with actual ABI
 
 function App() {
+  const { activateBrowserWallet, account, library } = useEthers();
+  const { state: depositState, send: deposit } = useContractFunction({
+    abi: placeholderABI,
+    address: placeholderContractAddress,
+    method: 'deposit',
+    args: [], // Replace with the method arguments if any
+  });
+  const { state: withdrawState, send: withdraw } = useContractFunction({
+    abi: placeholderABI,
+    address: placeholderContractAddress,
+    method: 'withdraw',
+    args: [], // Replace with the method arguments if any
+  });
+
   const [loading, setLoading] = useState(true);
   const [infoPopupOpen, setInfoPopupOpen] = useState(false);
-  const [ethAddress, setEthAddress] = useState(null);
-  const [web3, setWeb3] = useState(null);
-  const [contract, setContract] = useState(null);
-  const [hasProvider, setHasProvider] = useState(null);
-  const initialState = { accounts: [], chainId: "" };
-  const [wallet, setWallet] = useState(initialState);
-  const [isConnecting, setIsConnecting] = useState(false);
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -40,149 +48,79 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
 
-  useEffect(() => {
-    if (window.ethereum) {
-      window.ethereum.request({ method: 'eth_requestAccounts' }).then(accounts => {
-        if (accounts.length > 0) {
-          setEthAddress(accounts[0]);
+  const connectWallet = async () => {
+    const provider = await detectEthereumProvider();
+
+    if (provider) {
+      try {
+        await provider.request({ method: 'wallet_switchEthereumChain', params: [{ chainId: '1287' }] });
+      } catch (switchError) {
+        if (switchError.code === 4902) {
+          try {
+            await provider.request({
+              method: 'wallet_addEthereumChain',
+              params: [
+                {
+                  chainId: '1285',
+                  chainName: 'Moonriver',
+                  nativeCurrency: {
+                    name: 'Moonriver',
+                    symbol: 'MOVR',
+                    decimals: 18,
+                  },
+                  rpcUrls: ['https://moonriver.api.onfinality.io/public-ws'],
+                  blockExplorerUrls: ['https://moonriver.moonscan.io/'],
+                },
+              ],
+            });
+          } catch (addError) {
+            console.error('Failed to add Moonriver network');
+          }
         }
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    if (ethAddress) {
-      const web3Instance = new Web3(window.ethereum);
-      const contractInstance = new web3Instance.eth.Contract('your contract ABI here', 'your contract address here');
-      setWeb3(web3Instance);
-      setContract(contractInstance);
-    }
-  }, [ethAddress]);
-
-  useEffect(() => {
-    const getProvider = async () => {
-      const provider = await detectEthereumProvider({ silent: true });
-      setHasProvider(Boolean(provider));
-
-      if (provider) {
-        const accounts = await window.ethereum.request(
-          { method: 'eth_accounts' }
-        )
-        refreshAccounts(accounts)
-        window.ethereum.on('accountsChanged', refreshAccounts)
-        window.ethereum.on('chainChanged', refreshChain)
       }
+
+      await activateBrowserWallet();
     }
+  };
 
-    const refreshAccounts = (accounts) => {
-      if (accounts.length > 0) {
-        updateWallet(accounts)
-      } else {
-        setWallet(initialState)
-      }
-    }
-
-    const refreshChain = (chainId) => {
-      setWallet((wallet) => ({ ...wallet, chainId }))
-    }
-
-    getProvider()
-
-    return () => {
-      if(window.ethereum) {
-        window.ethereum.removeListener('accountsChanged', refreshAccounts)
-        window.ethereum.removeListener('chainChanged', refreshChain)
-      }
-    }
-  }, []);
-
-  const updateWallet = async (accounts) => {
-    if(window.ethereum) {
-      let balance = await window.ethereum.request({
-        method: "eth_getBalance",
-        params: [accounts[0], "latest"],
-      })
-      balance = web3.utils.fromWei(balance, 'ether');  // Convert from Wei to Ether
-      const chainId = await window.ethereum.request({
-        method: "eth_chainId",
-      })
-      setWallet({ accounts, balance, chainId });
-    }
-  }
-  
-  const handleConnect = async () => {
-    setIsConnecting(true)
-    if(window.ethereum) {
-      await window.ethereum.request({
-        method: "eth_requestAccounts",
-      })
-      .then((accounts) => {
-        setError(false)
-        updateWallet(accounts)
-      })
-      .catch((err) => {
-        setError(true)
-        setErrorMessage(err.message)
-      })
-      setIsConnecting(false)
-    }
-  }
-
-// Function to get Discord user ID
-const getDiscordUserId = async () => {
-  const response = await fetch('https://tipjar-back.vercel.app/api/main', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      /* any necessary data for the backend request, such as a Discord auth code */
-    })
-  });
-
-  const data = await response.json();
-
-  return data.discord_id;  // or whatever field contains the Discord user ID in the response
-};
-
-// Function to save user data
-const saveUserData = async () => {
-  if (ethAddress) {
-    const actualDiscordUserId = await getDiscordUserId();
-
-    fetch('https://tipjar-back.vercel.app/api/main', {
+  // Function to get Discord user ID
+  const getDiscordUserId = async () => {
+    const response = await fetch('https://tipjar-back.vercel.app/api/main', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        discord_id: actualDiscordUserId, 
-        ethereum_address: ethAddress
+        /* any necessary data for the backend request, such as a Discord auth code */
       })
-    }).then(response => {
-      // handle response
-    }).catch(error => {
-      // handle error
     });
-  }
-};
 
-const deposit = () => {
-  const amount = web3.utils.toWei('0.0000000000000001', 'ether'); 
-  contract.methods.deposit(amount.toString()).send({ from: ethAddress })
-    .on('transactionHash', function(hash) {
-      console.log(hash);
-    });
-}
+    const data = await response.json();
 
-const withdraw = () => {
-  // You need to adjust the amount according to your needs 
-  const amount = web3.utils.toWei('0.0000000000000001', 'ether'); 
-  contract.methods.withdraw(amount.toString()).send({ from: ethAddress })
-    .on('transactionHash', function(hash) {
-      console.log(hash);
-    });
-}
+    return data.discord_id;  // or whatever field contains the Discord user ID in the response
+  };
+
+  // Function to save user data
+  const saveUserData = async () => {
+    if (account) {
+      const actualDiscordUserId = await getDiscordUserId();
+
+      fetch('https://tipjar-back.vercel.app/api/main', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          discord_id: actualDiscordUserId, 
+          ethereum_address: account
+        })
+      }).then(response => {
+        // handle response
+      }).catch(error => {
+        // handle error
+      });
+    }
+  };
 
   return (
     <div className="App">
@@ -203,30 +141,41 @@ const withdraw = () => {
               onClick={() => setInfoPopupOpen(true)}
             />
             {infoPopupOpen && (
-          <div className="popup-container">
-            <div className="popup-content">
-              <h2>Project Description</h2>
-              <p>Add your project description here.</p>
-              <button onClick={() => setInfoPopupOpen(false)}>Close</button>
-            </div>
-          </div>
-        )}
+              <div className="popup-container">
+                <div className="popup-content">
+                  <h2>Project Description</h2>
+                  <p>Add your project description here.</p>
+                  <button onClick={() => setInfoPopupOpen(false)}>Close</button>
+                </div>
+              </div>
+            )}
             <div className="button-container">
-                    <img
-                      src={loginImage}
-                      alt="Login"
-                      className="login-button"
-                      onClick={saveUserData} // Call saveUserData when the login button is clicked
-                    />
-                  </div>
-            
+              <img
+                src={loginImage}
+                alt="Login"
+                className="login-button"
+                onClick={connectWallet}
+              />
+            </div>
           </div>
           <div className="tipjar-container">
             <img src={tipjarImage} alt="Tip Jar" />
           </div>
           <div className="button-container">
-            <img src={depositImage} alt="Deposit" className="deposit-button" onClick={deposit} />
-            <img src={withdrawImage} alt="Withdraw" className="withdraw-button" onClick={withdraw} />
+            <img
+              src={depositImage}
+              alt="Deposit"
+              className="deposit-button"
+              onClick={deposit}
+              disabled={depositState.status === 'Mining'}
+            />
+            <img
+              src={withdrawImage}
+              alt="Withdraw"
+              className="withdraw-button"
+              onClick={withdraw}
+              disabled={withdrawState.status === 'Mining'}
+            />
           </div>
           <div className="statistics-container">
             <img src={allStatsImage} alt="All Stats" className="all-stats" />
@@ -239,30 +188,60 @@ const withdraw = () => {
               <span className="statistic-number">5</span>
             </div>
             <div className="statistics-row">
-              <img src={depositedImage} alt="Deposited" className="deposited-image" data-statistic="100" />
+              <img
+                src={depositedImage}
+                alt="Deposited"
+                className="deposited-image"
+                data-statistic="100"
+              />
               <span className="statistic-number">100</span>
             </div>
             <div className="statistics-row">
-              <img src={withdrawnImage} alt="Withdrawn" className="withdrawn-image" data-statistic="50" />
+              <img
+                src={withdrawnImage}
+                alt="Withdrawn"
+                className="withdrawn-image"
+                data-statistic="50"
+              />
               <span className="statistic-number">50</span>
             </div>
           </div>
           <div className="my-stats-container">
             <img src={myStatsImage} alt="My Stats" className="my-stats" />
             <div className="statistics-row">
-              <img src={tipsGivenImage} alt="Tips Given" className="tips-given-image" data-statistic="20" />
+              <img
+                src={tipsGivenImage}
+                alt="Tips Given"
+                className="tips-given-image"
+                data-statistic="20"
+              />
               <span className="statistic-number">20</span>
             </div>
             <div className="statistics-row">
-              <img src={tipsReceivedImage} alt="Tips Received" className="tips-received-image" data-statistic="15" />
+              <img
+                src={tipsReceivedImage}
+                alt="Tips Received"
+                className="tips-received-image"
+                data-statistic="15"
+              />
               <span className="statistic-number">15</span>
             </div>
             <div className="statistics-row">
-              <img src={depositedImage} alt="Deposited" className="deposited-image" data-statistic="200" />
+              <img
+                src={depositedImage}
+                alt="Deposited"
+                className="deposited-image"
+                data-statistic="200"
+              />
               <span className="statistic-number">200</span>
             </div>
             <div className="statistics-row">
-              <img src={withdrawnImage} alt="Withdrawn" className="withdrawn-image" data-statistic="100" />
+              <img
+                src={withdrawnImage}
+                alt="Withdrawn"
+                className="withdrawn-image"
+                data-statistic="100"
+              />
               <span className="statistic-number">100</span>
             </div>
           </div>
